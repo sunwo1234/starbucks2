82f69a0ed0d547515a0be2eb640d1743ca917959
sunwo1234 <sunwo5678@gmail.com>
2022-02-23 17:37:38 +0900

    배치가 이상해요

--- js/main.js
+++ js/main.js
@@ -14,3 +14,28 @@ searchInputEl.addEventListener('blur', function (){
   searchEl.classList.remove('focused');               
   searchInputEl.setAttribute('placeholder', '');
 })
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function (){
+  console.log('scrool');
+  if (window.scrollY > 500) {
+ 
+    // 할 수 있는 것 : 배지 숨기기 
+ //gsap.to(요소, 지속시간, 옵션)
+ gsap.to(badgeEl, .6, {
+ opacity: 0,
+ display: 'none'
+ });
+ 
+} else {
+   badgeEl.style.disply = 'block';
+   sap.to(badgeEl, .6, {
+    opacity: 1,
+    display: 'block'
+    });
+  
+   //배지 보이기 
+ }
+}, 300));
+// _.throttle(함수, 시간)
